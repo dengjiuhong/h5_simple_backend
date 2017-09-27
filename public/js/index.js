8e673912ea1d353bc34106a98452ad013fe160a0
zhengbaic <342527880@qq.com>
2017-09-27 15:54:36 +0800

    wx_debug

--- public/js/index.js
+++ public/js/index.js
@@ -39,21 +39,20 @@ function wx_process(data) {
         });
   wx.ready(function(){
             wx.onMenuShareTimeline({
-                title: 'test', // 分享标题
-                link: 'www.google.com', // 分享链接
-                imgUrl: '', // 分享图标
+                title: '大头看见了吗', // 分享标题
+                link: 'http://oppo10.nplusgroup.net/', // 分享链接
                 success: function () { 
                     // 用户确认分享后执行的回调函数
+                    alert('分享成功');
                 },
                 cancel: function () { 
                     // 用户取消分享后执行的回调函数
+                    alert('分享失败');
                 }
             });
             wx.onMenuShareAppMessage({
-                title: 'JasonFF', // 分享标题
-                desc: 'JasonFF的主页', // 分享描述
-                link: 'www.google.com', // 分享链接
-                imgUrl: '', // 分享图标
+                title: '大头看见了吗', // 分享标题
+                link: 'http://oppo10.nplusgroup.net/', // 分享链接
                 type: 'link', // 分享类型,music、video或link，不填默认为link
                 dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
                 success: function () { 
