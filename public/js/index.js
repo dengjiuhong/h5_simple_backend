9045c874754ee21c2f6a7000280630b1dda1e6f5
zhengbaic <342527880@qq.com>
2017-09-27 15:46:51 +0800

    wx_debug

--- public/js/index.js
+++ public/js/index.js
@@ -49,6 +49,20 @@ function wx_process(data) {
                     // 用户取消分享后执行的回调函数
                 }
             });
+            wx.onMenuShareAppMessage({
+                title: 'JasonFF', // 分享标题
+                desc: 'JasonFF的主页', // 分享描述
+                link: 'www.google.com', // 分享链接
+                imgUrl: '', // 分享图标
+                type: 'link', // 分享类型,music、video或link，不填默认为link
+                dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                success: function () { 
+                    // 用户确认分享后执行的回调函数
+                },
+                cancel: function () { 
+                    // 用户取消分享后执行的回调函数
+                }
+            });
           });
 }
 function requestPic() {
