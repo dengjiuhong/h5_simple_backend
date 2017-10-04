56df2d170065a46321964c7c7eec6dfcd4d4fc05
zhengbaic <342527880@qq.com>
2017-10-05 00:33:17 +0800

    1

--- public/js/index.js
+++ public/js/index.js
@@ -248,7 +248,7 @@ function page2() {
     });
     $("#share_in").css("display", "none");
     document.getElementById("share_in").addEventListener("click", function() {
-        $("#close_0").removeAttr("loop");
+        //$("#close_0").removeAttr("loop");
         $("#close_0").get(0).addEventListener("timeupdate",function(){
           if($("#close_0").get(0).ended){
             $("#close_0").get(0).pause();
