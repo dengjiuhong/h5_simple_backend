d8207c307180b01db532acd4f7fe2da24526828a
zhengbaic <342527880@qq.com>
2017-10-06 01:31:54 +0800

    1

--- public/js/index.js
+++ public/js/index.js
@@ -299,7 +299,7 @@ function page2() {
     .css({
       "position": "absolute",
       "width": BG_WIDTH / 3,
-      "height": BG_HEIGHT / 3.95,
+      "height": BG_HEIGHT / 3.93,
       "left": BG_WIDTH / 3,
       "top": BG_HEIGHT / 2.75,
     })
