9071aeb0ddaf8cc5bed2926d81a5cd2d83af3961
zhengbaic <342527880@qq.com>
2017-10-06 04:04:02 +0800

    1

--- public/js/index.js
+++ public/js/index.js
@@ -377,13 +377,13 @@ function page2() {
       //全景消失
       $("#close").css("opacity", "1");
       $("#close").animate({"opacity": "0"}, 1500);
-      $("#close").css("display", "none");
-      $("#close").css("opacity", "1");
       $("#light_word").css("display", "none");
       $("#panorama_4").animate({"opacity": "0"}, 1500);
       $("#panorama_5").animate({"opacity": "0"}, 1500);
       $("#panorama_6").animate({"opacity": "0"}, 1500);
       setTimeout(function() {
+          $("#close").css("display", "none");
+          $("#close").css("opacity", "1");
           $("#close_0").css("opacity", "0"); //视频渐亮
           $("#close_0").css("display", "block");
           $("#close_0").animate({"opacity": "1"}, 750, function() {
