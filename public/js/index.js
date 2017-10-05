2f08544977d2b9e9d7b3fa38bf139a50c2990364
zhengbaic <342527880@qq.com>
2017-10-05 15:42:02 +0800

    蠢

--- public/js/index.js
+++ public/js/index.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
       v2.get(0).pause();
       setTimeout(function(){
             $("#in").fadeOut();
-            $("#welcome".fadeOut();
+            $("#welcome").fadeOut();
             $(".p1").css("display", "none");
             $(".p3").css("display", "none");
             $(".p0").css("display", "block");
