346980d38bd3d9c25da6e693e5e35bec45d93940
Zing22 <leezing22@gmail.com>
2017-10-05 20:12:43 +0800

    加载页

--- public/js/index.js
+++ public/js/index.js
@@ -9,9 +9,40 @@ var audio;
 
 // 顶栏高度
 var topHeight = window.screen.height - window.innerHeight;
+$(document).ready(function() {
+  $(".page.loading").show();
+  var loader = new resLoader({
+    resources : [
+         'http://p2.qhimg.com/t01ed1438874f940dc0.jpg',
+         'http://p9.qhimg.com/t01b4ff03b72c7dc6c7.jpg',
+         'http://p2.qhimg.com/t01dd90dfbec92074d0.jpg',
+         'http://p7.qhimg.com/t01cfec6d87cde457c5.jpg',
+         'http://p9.qhimg.com/t01943ced462da67833.jpg',
+         'http://p0.qhimg.com/t01943ced462da67833.jpg',
+         'http://p6.qhimg.com/t01aa15a7ba7ccb49a7.jpg',
+         'http://p8.qhimg.com/t010f1e8badf1134376.jpg',
+         'http://p8.qhimg.com/t01cf37ea915533a032.jpg',
+         'http://p3.qhimg.com/t0193d8a3963e1803e9.jpg',
+         'http://p3.qhimg.com/t01cd6a4d4b4bd4457b.jpg'
+    ],
+    onStart : function(total){
+         console.log('start:' + total);
+    },
+    onProgress : function(current, total){
+      console.log(current+'/'+total);
+      var percent = current/total*100;
+      $('.loading > .progress > .bar').css('width', percent+'%');
+    },
+    onComplete : function(total){
+      $(".page.loading").fadeOut();
+      main();
+      // alert("加载完了！");
+    }
+  });
+  loader.start();
+});
 
-
-$(document).ready(function(){
+function main() {
   panorama = Math.floor(Math.random()*3);
   preload(panorama);
   $(".p0").fadeIn("fast");
@@ -72,7 +103,7 @@ $(document).ready(function(){
         wx_process(wx_data);
       }
     });
-});
+}
 
 function preload(panorama) {
   for(var i = 4; i < 7; i++) {
