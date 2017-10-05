b7af6f8a7d90a90b7962354a02e04298546662f3
zhengbaic <342527880@qq.com>
2017-10-05 21:49:30 +0800

    预加载

--- public/js/index.js
+++ public/js/index.js
@@ -14,17 +14,19 @@ $(document).ready(function() {
   $(".page.loading").show();
   var loader = new resLoader({
     resources : [
-         'http://p2.qhimg.com/t01ed1438874f940dc0.jpg',
-         'http://p9.qhimg.com/t01b4ff03b72c7dc6c7.jpg',
-         'http://p2.qhimg.com/t01dd90dfbec92074d0.jpg',
-         'http://p7.qhimg.com/t01cfec6d87cde457c5.jpg',
-         'http://p9.qhimg.com/t01943ced462da67833.jpg',
-         'http://p0.qhimg.com/t01943ced462da67833.jpg',
-         'http://p6.qhimg.com/t01aa15a7ba7ccb49a7.jpg',
-         'http://p8.qhimg.com/t010f1e8badf1134376.jpg',
-         'http://p8.qhimg.com/t01cf37ea915533a032.jpg',
-         'http://p3.qhimg.com/t0193d8a3963e1803e9.jpg',
-         'http://p3.qhimg.com/t01cd6a4d4b4bd4457b.jpg'
+         '/image/close.png',
+         '/image/brand.png',
+         '/image/brand_.png',
+         '/image/brand_.png',
+         '/image/panorama/0/4.png',
+         '/image/panorama/0/5.png',
+         '/image/panorama/0/6.png',
+         '/image/panorama/1/4.png',
+         '/image/panorama/1/5.png',
+         '/image/panorama/1/6.png',
+         '/image/panorama/2/4.png',
+         '/image/panorama/2/5.png',
+         '/image/panorama/2/6.png'
     ],
     onStart : function(total){
          console.log('start:' + total);
