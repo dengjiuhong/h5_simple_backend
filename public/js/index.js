a7ff80ac4112c234cf9960c8c86a8051956a9f9b
zhengbaic <342527880@qq.com>
2017-10-06 06:25:03 +0800

    1

--- public/js/index.js
+++ public/js/index.js
@@ -471,11 +471,11 @@ function page2() {
     });
     var gif_lis = function() {
       $("#share_gif").css("display", "none");
-      document.getElementsByClassName("p3").removeEventListener("click", gif_lis);
+      document.getElementsByClassName("p3")[0].removeEventListener("click", gif_lis);
     }
     $(".share_my").click(function() {
       $("#share_gif").css("display", "block");
-      document.getElementsByClassName("p3").addEventListener("click", gif_lis);
+      document.getElementsByClassName("p3")[0].addEventListener("click", gif_lis);
     })
     var lastMouseX = 0,
       lastMouseY = 0,
