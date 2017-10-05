ecd43c16595e1d5295e45d22018974bc8eb621cf
Zing22 <leezing22@gmail.com>
2017-10-05 21:46:04 +0800

    吊牌

--- public/js/index.js
+++ public/js/index.js
@@ -11,20 +11,17 @@ var close_lock = 0;
 // 顶栏高度
 var topHeight = window.screen.height - window.innerHeight;
 $(document).ready(function() {
+  // debug //
+  // $(".page.p1").show();
+  // $(".upload_wrap").animate({"margin-top": "0"}, 1000);
+  // $("#in, #welcome").fadeIn();
+  // return false;
+  // debug //
   $(".page.loading").show();
   var loader = new resLoader({
     resources : [
          'http://p2.qhimg.com/t01ed1438874f940dc0.jpg',
-         'http://p9.qhimg.com/t01b4ff03b72c7dc6c7.jpg',
-         'http://p2.qhimg.com/t01dd90dfbec92074d0.jpg',
-         'http://p7.qhimg.com/t01cfec6d87cde457c5.jpg',
-         'http://p9.qhimg.com/t01943ced462da67833.jpg',
-         'http://p0.qhimg.com/t01943ced462da67833.jpg',
-         'http://p6.qhimg.com/t01aa15a7ba7ccb49a7.jpg',
-         'http://p8.qhimg.com/t010f1e8badf1134376.jpg',
-         'http://p8.qhimg.com/t01cf37ea915533a032.jpg',
-         'http://p3.qhimg.com/t0193d8a3963e1803e9.jpg',
-         'http://p3.qhimg.com/t01cd6a4d4b4bd4457b.jpg'
+         'http://p9.qhimg.com/t01b4ff03b72c7dc6c7.jpg'
     ],
     onStart : function(total){
          console.log('start:' + total);
@@ -79,7 +76,8 @@ function main() {
       setTimeout(function() {
         $(".wrap").removeClass("p1-fake");
       }, 1000);
-      $(".upload_wrap").animate({"margin-top": "" + (0-topHeight) + "px"}, 3000);
+      $(".upload_wrap").animate({"margin-top": "0"}, 3000);
+      $("#in, #welcome").fadeIn();
       //v2.get(0).play();
     }
   });
@@ -97,9 +95,10 @@ function main() {
         },2000);
       $("#in").fadeOut();
       $("#welcome").fadeOut();
-      $(".upload_wrap").animate({"margin-top": "-470px"}, 2000, function(){
+      $(".upload_wrap").animate({"margin-top": "-120vw"}, 2000, function(){
         $(".upload_wrap").css("display", "none");
       });
+      $("#in, #welcome").fadeOut();
       v2.get(0).addEventListener("timeupdate",function(){
         if(v2.get(0).ended){
           v2.css("display", "none");
