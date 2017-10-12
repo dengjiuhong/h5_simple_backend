a2ae352ef2c9caadc18d7e580f6318af794e29c1
zhengbaic <342527880@qq.com>
2017-10-12 10:56:46 +0800

    1

--- public/js/index.js
+++ public/js/index.js
@@ -332,6 +332,8 @@ function main() {
 //   }
 // };
 function wx_process(data) {
+  alert(user_name);
+  alert(encodeURI(user_name));
   wx.config({
     debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: data.appid, // 必填，公众号的唯一标识
