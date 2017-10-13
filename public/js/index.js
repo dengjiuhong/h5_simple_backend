6d660e621bdf0521adfcdf0e087f08ac31c46d20
zhengbaic <342527880@qq.com>
2017-10-13 21:14:14 +0800

    1

--- public/js/index.js
+++ public/js/index.js
@@ -348,7 +348,7 @@ function main() {
 // };
 function wx_process(data) {
   wx.config({
-    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: data.appid, // 必填，公众号的唯一标识
     timestamp: data.timestamp, // 必填，生成签名的时间戳
     nonceStr: data.random_str, // 必填，生成签名的随机串
@@ -358,7 +358,7 @@ function wx_process(data) {
   wx.ready(function () {
     wx.onMenuShareTimeline({
       title: '看看' + user_name +'的博物馆！', // 分享标题
-      link: 'http://wx.oppo.com/oppootherfirm10/my_museum?name=' + encodeURI(user_name) + '&museum=' + panorama + '&time=' + user_time + '&id=' + user_id, // 分享链接
+      link: 'http://oppo10.nplusgroup.net/my_museum?name=' + encodeURI(user_name) + '&museum=' + panorama + '&time=' + user_time + '&id=' + user_id, // 分享链接
       imgUrl: "", // 分享图标
       success: function () {
         // 用户确认分享后执行的回调函数
@@ -370,7 +370,7 @@ function wx_process(data) {
     wx.onMenuShareAppMessage({
       title: '看看'+user_name+'的博物馆！', // 分享标题
       desc: '这是描述', // 分享描述
-      link: 'http://wx.oppo.com/oppootherfirm10/my_museum?name=' + encodeURI(user_name) + '&museum=' + panorama + '&time=' + user_time + '&id=' + user_id, // 分享链接
+      link: 'http://oppo10.nplusgroup.net/my_museum?name=' + encodeURI(user_name) + '&museum=' + panorama + '&time=' + user_time + '&id=' + user_id, // 分享链接
       imgUrl: "", // 分享图标
       type: 'link', // 分享类型,music、video或link，不填默认为link
       dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
