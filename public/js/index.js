a464a1f67a296da4ad3ca65fa633af912a085b67
zhengbaic <342527880@qq.com>
2017-10-21 17:34:57 +0800

    1

--- public/js/index.js
+++ public/js/index.js
@@ -605,7 +605,7 @@ function wx_process() {
   wx.ready(function () {
     wx.onMenuShareTimeline({
       title: '快来看' + user_name +'的照片博物馆', // 分享标题
-      link: 'http://wx.oppo.com/oppootherfirm10/my_museum?name=' + encodeURI(user_name) + '&museum=' + panorama + '&time=' + user_time + '&id=' + user_id + '&code=' + $("#wx_code").val(), // 分享链接
+      link: 'http://wx.oppo.com/oppootherfirm10/my_museum?name=' + encodeURI(user_name) + '&museum=' + panorama + '&time=' + user_time + '&id=' + user_id + '&code=' + GetQueryString("code"), // 分享链接
       imgUrl: "http://oppo10.nplusgroup.net/image/500px.jpg", // 分享图标
       success: function () {
         // 用户确认分享后执行的回调函数
@@ -617,7 +617,7 @@ function wx_process() {
     wx.onMenuShareAppMessage({
       title: '快来看'+user_name+'的照片博物馆', // 分享标题
       desc: '里面有神奇的...', // 分享描述
-      link: 'http://wx.oppo.com/oppootherfirm10/my_museum?name=' + encodeURI(user_name) + '&museum=' + panorama + '&time=' + user_time + '&id=' + user_id + '&code=' + $("#wx_code").val(), // 分享链接
+      link: 'http://wx.oppo.com/oppootherfirm10/my_museum?name=' + encodeURI(user_name) + '&museum=' + panorama + '&time=' + user_time + '&id=' + user_id + '&code=' + GetQueryString("code"), // 分享链接
       imgUrl: "http://oppo10.nplusgroup.net/image/500px.jpg",// 分享图标
       type: 'link', // 分享类型,music、video或link，不填默认为link
       dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
