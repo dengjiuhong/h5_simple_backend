0fa9d427e9eac5655cc97375a54cf86baef4dd24
zhengbaic <342527880@qq.com>
2017-10-22 13:22:56 +0800

    黑屏

--- public/js/index.js
+++ public/js/index.js
@@ -118,6 +118,7 @@ $(document).ready(function () {
   }
 
     $(".wrap").addClass("p0-fake");
+    $(".wrap").css("background-color", "#000000");
   platform = window.location.pathname;
   platform = platform.substr(1, platform.length);
   if(platform == "weibo") {
